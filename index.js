43b615606ef78bfd0a873e4bac89c7073c724e82
Sagar-57 <sagar05.ms@gmail.com>
2021-10-20 22:47:17 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,33 +3,52 @@ import Movies from './movies';
 
 import { render } from 'react-dom';
 
-class App extends Component{
+class App extends Component {
   state = {
-    movies: []
-  }
-  
-  async componentDidMount(){
+    movies: [],
+  };
+
+  async componentDidMount() {
+    
     try {
-      const res = await  fetch('https://api.themoviedb.org/3/discover/movie?api_key=2f59754079cb51126497ed7754eef01f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const res = await fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=2f59754079cb51126497ed7754eef01f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+      );
 
       const movies = await res.json();
       this.setState({
-        movies: movies.results
+        movies: movies.results,
       });
-      console.log(movies);
-    } catch(e){
+    } catch (e) {
       console.log(e);
     }
+   
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
-          {this.state.movies.map((movie)=> <Movies key={movie.id} movie={movie}/>)}
+        {this.state.movies.map((movie) => (
+          <Movies key={movie.id} movie={movie} />
+        ))}
       </div>
     );
   }
 }
-
+class MovieInfo extends Component {
+  render() {
+    const { id } = this.props.match.params;
+    return (
+      <div>
+        <AdditionalInfo id={id} />
+        <Router>
+          <Route path="/" exact component={Index} />
+          <Route path="/movieInfo/:id" exact component={MovieInfo} />
+          <Route path="/users/" component={Users} />
+        </Router>
+      </div>
+    );
+  }
+}
 
 render(<App />, document.getElementById('root'));
